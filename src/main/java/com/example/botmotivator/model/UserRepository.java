faf30fe9f7de1f2bcd99430aa65db537476bbcea
DonToli <strt87@yandex.ru>
2022-10-06 13:24:20 +0300

    Добавил базу данных PostgreSQL, создал таблицу и добавил смайлы.

---
+++ src/main/java/com/example/botmotivator/model/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.botmotivator.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Long> {
+}
--- src/main/java/com/example/botmotivator/service/TelegramBot.java
+++ src/main/java/com/example/botmotivator/service/TelegramBot.java
@@ -1,16 +1,25 @@
 package com.example.botmotivator.service;
 
 import com.example.botmotivator.config.BotConfig;
+import com.example.botmotivator.model.User;
+import com.example.botmotivator.model.UserRepository;
+import com.vdurmont.emoji.EmojiParser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +27,8 @@ import java.util.List;
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
+    @Autowired
+    private UserRepository userRepository;
     static final String HELP_TEXT = "This bot is created to demonstrate capabilities.\n\n" +
             "You can execute commands from the main menu on the left or the by typing a command: \n\n" +
             "Type /start to see a welcome message\n\n";
@@ -60,7 +71,11 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start":
+                    registerUser(update.getMessage());
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/register":
+
                     break;
                 case "/help":
                     sendMessage(chatId,HELP_TEXT);
@@ -71,12 +86,28 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private void registerUser(Message message) {
+        if (userRepository.findById(message.getChatId()).isEmpty()){
+            var chatId = message.getChatId();
+            var chat = message.getChat();
+
+            User user = new User();
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegistredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("user saved: "+user);
+        }
+    }
+
     private void startCommandReceived(long chatId, String name) {
 
-        String answer = "Привет, " + name + ", для правильной настройки твоего бота мне надо задать тебе 3 вопроса!\n\n " +
-                " На какую цель не хватает мотивации?";
+        String answer = EmojiParser.parseToUnicode ("Привет, " + name + ", для правильной настройки твоего бота мне надо задать тебе 3 вопроса :blush: !\n\n " +
+                " На какую цель не хватает мотивации?");
         log.info("Replied to user " + name);
-
         sendMessage(chatId, answer);
     }
 
@@ -85,6 +116,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSent);
 
+        //keyboardForStart(message);
+
         try {
             execute(message);
         } catch (TelegramApiException e) {
@@ -92,5 +125,23 @@ public class TelegramBot extends TelegramLongPollingBot {
             log.error("Error occurred: " + e.getMessage());
         }
 
+    }
+
+    private static void keyboardForStart(SendMessage message) {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup(); // разметка для клавиатуры
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();  //список рядов для добавления кнопок
+        KeyboardRow row = new KeyboardRow(); //создаем ряд
+        row.add("Тема мотивации"); // добавляем
+        row.add("Время показа"); // добавляем
+        keyboardRows.add(row);// добавляем в список рядов
+        row = new KeyboardRow();//создаём новый ряд
+        row.add("Количество");
+        row.add("Формат крнтента");
+        keyboardRows.add(row);
+
+        keyboardMarkup.setKeyboard(keyboardRows);//Добавили к клавиатуре ряды
+
+        message.setReplyMarkup(keyboardMarkup); // Привязали к сообщению ряды
     }
 }
